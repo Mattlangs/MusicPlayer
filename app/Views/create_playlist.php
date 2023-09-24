594b7456b0306b42ca3b48f184ce5aa9179b091d
Mattlangs <balmesmatthew85@gmail.com>
2023-09-24 06:44:51 -0900

    hehe

---
+++ app/Views/create_playlist.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <!-- create_playlist.php -->
+<form method="post" action="/create_playlist" class="modal fade" id="createPlaylist">
+    <input type="text" name="playlist_name" placeholder="Playlist Name">
+    <button type="submit">Create Playlist</button>
+</form>
+</body>
+</html>
